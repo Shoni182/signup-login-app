03a28d9007a6846b52ba089394a7dead869b629d
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-08-07 15:17:44 +0300

    add error and MongoDB components

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { error } from 'celebrate';
+import { celebrate } from 'celebrate';
+// DB
+import { connectMongoDB } from './db/connectMongoDB';
+// middlewares
+import { errorHandler } from './middleware/errorHandler';
+// import {notFoundHandler}from
+
+const app = express();
+const PORT = 3000;
+
+// Перший маршрут
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello world!' });
+});
+
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
